1ad9731a0b49776a553155e906965a40e8a86f79
Justy <justynaonceagainforgothermail@gmail.com>
2024-10-15 22:55:09 +0200

    added Calculator

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Calculator {
+    public double addingIntegers(double x,double y){
+        return x +y;
+    }
+    public double subtractingIntegers(double x,double y){
+        return x-y;
+    }
+
+}
